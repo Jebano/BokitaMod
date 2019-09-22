9f3286e17fe78bd0f9d89192eea63a07ae90a00f
Jebano <55359479+Jebano@users.noreply.github.com>
2019-09-22 00:11:58 -0300

    Add files via upload

--- src/main/java/jebano/bokitamod/item/PepperSpray.java
+++ src/main/java/jebano/bokitamod/item/PepperSpray.java
@@ -1,36 +1,39 @@
 package jebano.bokitamod.item;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 import net.minecraft.stats.Stats;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvents;
-import net.minecraft.world.World;
 
 public class PepperSpray extends Item {
 	public PepperSpray(Item.Properties properties) {
 		super(properties.maxDamage(20));
 	}
-	
-	   public boolean itemInteractionForEntity(ItemStack stack, World worldIn, net.minecraft.entity.player.PlayerEntity playerIn, LivingEntity entity, net.minecraft.util.Hand hand) {
-	         if (entity.world.isRemote) return false;
-	         if (entity instanceof LivingEntity) {
-	        	 entity.addPotionEffect(new EffectInstance(Effects.POISON, 100));
-	        	 entity.addPotionEffect(new EffectInstance(Effects.BLINDNESS, 100));
-	        	 entity.addPotionEffect(new EffectInstance(Effects.WEAKNESS, 20));
-	            if (stack.getMaxDamage() - stack.getDamage() >= 1) {
-	               stack.damageItem(1, playerIn, (e) -> {e.sendBreakAnimation(hand);});}
+
+	   @Override
+	   public boolean itemInteractionForEntity(ItemStack stack, PlayerEntity playerIn, LivingEntity target, net.minecraft.util.Hand hand) {
+		   if (target.world.isRemote) return false;
+	         if (target instanceof LivingEntity) {
+	        	 target.addPotionEffect(new EffectInstance(Effects.POISON, 40));
+	        	 target.addPotionEffect(new EffectInstance(Effects.BLINDNESS, 125));
+	        	 target.addPotionEffect(new EffectInstance(Effects.WEAKNESS, 20));
+	        	 stack.damageItem(1, target, e -> e.sendBreakAnimation(hand));
+	        	 if (stack.isEmpty()) {
+	                 stack = new ItemStack(Items.ARROW);
+	              }
 	         }
 
-	         worldIn.playSound(playerIn, playerIn.getPosition(), SoundEvents.ENTITY_CREEPER_HURT, SoundCategory.PLAYERS, 0.8F, 0.8F);
-	         playerIn.getCooldownTracker().setCooldown(this, 30);
-	 		 playerIn.swingArm(hand);
-	 		 playerIn.setActiveHand(hand);
-	 		 playerIn.addStat(Stats.ITEM_USED.get(this));
+	        playerIn.getEntityWorld().playSound(null, playerIn.getPosition(), SoundEvents.ENTITY_CREEPER_HURT, SoundCategory.PLAYERS, 0.9f, 3.0f);
+	        playerIn.swingArm(hand);
+	 		playerIn.setActiveHand(hand);
+	 		playerIn.addStat(Stats.ITEM_USED.get(this));
 			return true;
 	      }
-	   }
-	
+	   
+}
